b4c76d7944ea960e1dce708b55a57e89bc7cf214
Aleš Máca <ales.maca92@gmail.com>
2025-01-08 20:19:41 +0100

    08/01/2025

--- src/app/contactform.js
+++ src/app/contactform.js
@@ -55,7 +55,7 @@ function ContactForm() {
     }
   };
   return (
-    <div className="max-w-3xl mx-auto p-6  rounded-lg shadow-lg">
+    <div className="max-w-3xl mx-auto p-6 ">
       <h2 className="titleH2">KONTAKTNÍ FORMULÁŘ</h2>
       <form className="grid grid-cols-1 md:grid-cols-2 gap-8">
         <div>
